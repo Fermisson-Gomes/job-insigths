2aa63cbd650b846de3421db4cebaf41397a6bcbf
Fermisson-Gomes <fermison.barbosa@gmail.com>
2023-01-23 15:40:11 -0300

    [feature] implementada as funções dos requisitos 2 e 3

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -5,44 +5,20 @@ from typing import List, Dict
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    with open(path, encoding = "utf-8") as file:
-        reader = csv.DictReader(file)
+    with open(path, encoding="utf-8") as file:
+        reader=csv.DictReader(file)
         return list(reader)
 
     raise NotImplementedError
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
+    data = read(path)
+    job_types=[job["job_type"] for job in data]
+    return list(set(job_types))
     raise NotImplementedError
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
+    
     raise NotImplementedError
